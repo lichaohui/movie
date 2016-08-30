a579f9357cf8fc1583ee5ad8254da41e215c10ac
lichaohui <158109640@qq.com>
2016-08-30 20:02:16 +0800

    test

---
+++ public/js/home/register.js
@@ -0,0 +1,10 @@
+$(function(){
+    var password=$("#password").val();
+    var passwordrepeat=$("#passwordrepeat").val();
+    if(password==passwordrepeat){
+        
+    }else{
+        alert('两次输入的密码不一致');
+        return false;
+    }
+})
