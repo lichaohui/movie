f42256d9ea5d8b7f61f724de841563fef16b0d13
lichaohui <158109640@qq.com>
2016-09-01 20:26:55 +0800

    test

--- app/controllers/admin/admin.js
+++ app/controllers/admin/admin.js
@@ -32,6 +32,7 @@ exports.doLogin=function(req,res){
                          * 所以session信息会在发生http请求的时候包含在请求体中
                          */
                         req.session.admin=data;
+                        console.log(req.session.admin);
                         res.json({'isError':false,'message':'登录成功，即将进入首页！！'});
                     }else{
                         //如果密码不匹配则返回错误信息
