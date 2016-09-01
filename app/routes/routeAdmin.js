4b306b55d5b51ef798a1824942952c6818c817d4
lichaohui <158109640@qq.com>
2016-09-01 22:08:23 +0800

    test

--- app/routes/routeAdmin.js
+++ app/routes/routeAdmin.js
@@ -1,7 +1,7 @@
 //引入后台控制器文件
 var movie=require('../controllers/admin/movie');
 var user=require('../controllers/admin/user');
-var admin=require('../controllers/admin/admin');
+var admin=require('../controllers/admin/admin')(app);
 
 module.exports=function(app){
     /*
