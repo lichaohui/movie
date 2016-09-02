d06b9c8855df1c61bfae6a879b373f7c2ca37f1c
lichaohui <158109640@qq.com>
2016-09-02 09:36:17 +0800

    test

--- public/js/admin/adminCreate.js
+++ public/js/admin/adminCreate.js
@@ -6,8 +6,10 @@ $(function(){
             url:url,
             success:function(data){
                 if(data.isError){
+                    $("#success").addClass('hidden');
                     $("#warning").text(data.message).removeClass('hidden');
                 }else{
+                    $("#warning").addClass('hidden');
                     $("#success").text(data.message).removeClass('hidden');
                 }
             },
