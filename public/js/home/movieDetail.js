d2bf338c24de4e87072b477bf36b96db9c9c1013
lichaohui <158109640@qq.com>
2016-09-02 15:19:05 +0800

    test

---
+++ public/js/home/movieDetail.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(".reply").toggle(function(){
+        $(this).siblings('.replyForm').removeClss('hidden');
+    },function(){
+        $(this).siblings('.replyForm').addClass('hidden');
+    });
+})
