5802ef0e4685b881803d13a8d10619c2dd3d7a2a
lichaohui <158109640@qq.com>
2016-09-02 16:44:51 +0800

    test

--- public/js/home/movieDetail.js
+++ public/js/home/movieDetail.js
@@ -2,8 +2,10 @@ $(function(){
     $('.reply').click(function(){
         var user=$("#user");
         if(user){
+            alert(user);
             alert('你已经登录了');
         }else{
+            alert(user);
             alert('对不起 你还没有登录过');
         }
         if($(this).siblings('.replyForm').hasClass('hidden')){
