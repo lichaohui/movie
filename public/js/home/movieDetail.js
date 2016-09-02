6eba8be66c1779befdf7d24039170e26f4c8dba3
lichaohui <158109640@qq.com>
2016-09-02 17:05:21 +0800

    test

--- public/js/home/movieDetail.js
+++ public/js/home/movieDetail.js
@@ -2,16 +2,13 @@ $(function(){
     $('.reply').click(function(){
         var user=$("#user").text();
         if(user){
-            alert(user);
-            alert('你已经登录了');
+            if($(this).siblings('.replyForm').hasClass('hidden')){
+                $(this).siblings('.replyForm').removeClass('hidden');
+            }else{
+                $(this).siblings('.replyForm').addClass('hidden');
+            }
         }else{
-            alert(user);
-            alert('对不起 你还没有登录过');
-        }
-        if($(this).siblings('.replyForm').hasClass('hidden')){
-            $(this).siblings('.replyForm').removeClass('hidden');
-        }else{
-            $(this).siblings('.replyForm').addClass('hidden');
+            alert('I\'m sorry you have not logged in, please login');
         }
     });
 })
