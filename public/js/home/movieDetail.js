c2851dd1142067fc3ffb8384b0c3d8c0453ccd96
lichaohui <158109640@qq.com>
2016-09-02 15:54:41 +0800

    test

--- public/js/home/movieDetail.js
+++ public/js/home/movieDetail.js
@@ -1,7 +1,7 @@
-$(function(){
+/*$(function(){
     $(".reply").toggle(function(){
         $(this).siblings('.replyForm').removeClss('hidden');
     },function(){
         $(this).siblings('.replyForm').addClass('hidden');
     });
-})
+})*/
