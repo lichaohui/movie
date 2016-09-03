992af8f3ef9ed6f951dc8862d5cb472bf01e6fae
lichaohui <158109640@qq.com>
2016-09-03 21:00:34 +0800

    d

--- public/js/home/movieDetail.js
+++ public/js/home/movieDetail.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('#content').scrollTop(400);
+    $('body').scrollTop(400);
     /*----异步加载更多评论---*/
     $("#viewMore").click(function(){
         var n=$("#comments li").length;
