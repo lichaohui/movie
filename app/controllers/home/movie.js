ec05003d80d21dc73fe4907ecc228df398a0c59e
lichaohui <158109640@qq.com>
2016-09-03 13:11:23 +0800

    test

--- app/controllers/home/movie.js
+++ app/controllers/home/movie.js
@@ -26,8 +26,6 @@ exports.show=function(req,res){
     //通过id获取数据并将数据发送给前台视图
     movie.findById(id,function(err,data){
         comment.findByMovie(id,function(err,comments){
-            console.log('aaa');
-            console.log(comments.length);
             res.render('home/movie/detail',{'title':'detail','movie':data,'comments':comments});
         });
     });
