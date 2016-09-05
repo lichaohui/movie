0de5f6be441db995f8f5e713f854018f9a11673e
lichaohui <158109640@qq.com>
2016-09-05 19:57:20 +0800

    d

--- public/js/home/movieDetail.js
+++ public/js/home/movieDetail.js
@@ -154,17 +154,22 @@ $(function(){
                 alert(data.message);
             }else{
                 var li;
-                //判断用户是否登录
-                var isLogin=$('#isLogin').val();
-                if(isLogin=='yes'){
-                    for(var i=0;i<data.replies.length;i++){
-                        li='<li class="list-group-item"><h5 class="list-group-item-heading"><b>'+data.replies[i].from.name+'</b> replied to <b>'+data.replies[i].toWho.name+'</b><time class="pull-right">'+data.replies[i].meta.created_at+'</time></h5><p class="list-group-item-text">'+data.replies[i].content+'</p><button type="button" class="reply btn btn-default btn-xs" >reply</button><form action="/reply/store" method="post" role="form" class="replyForm hidden"><input type="hidden" name="from" value=""><input type="hidden" name="toWho" value='+data.replies[i].from._id+'><input type="hidden" name="toWhichComment" value='+cid+'><input type="hidden" name="toWhichReply" value='+data.replies[i]._id+'><div class="form-group"><textarea name="content" placeholder="Please input your comment here" required="" class="form-control"></textarea></div><b></b> <button type="button" class="replyBtn btn btn-primary btn-xs" isToReply="yes">submit</button></form></li>';
-                        $("#replies-list").append(li);
-                    }
+                if(data.length==0){
+                    //如果没有回复则显示一个no reply
+                    li='<li class="list-group-item">No reply!</li>'
                 }else{
-                    for(var i=0;i<data.replies.length;i++){
-                        li='<li class="list-group-item"><h5 class="list-group-item-heading"><b>'+data.replies[i].from.name+'</b> replied to <b>'+data.replies[i].toWho.name+'</b><time class="pull-right">'+data.replies[i].meta.created_at+'</time></h5><p class="list-group-item-text">'+data.replies[i].content+'</p></li>';
-                        $("#replies-list").append(li);
+                    //判断用户是否登录
+                    var isLogin=$('#isLogin').val();
+                    if(isLogin=='yes'){
+                        for(var i=0;i<data.replies.length;i++){
+                            li='<li class="list-group-item"><h5 class="list-group-item-heading"><b>'+data.replies[i].from.name+'</b> replied to <b>'+data.replies[i].toWho.name+'</b><time class="pull-right">'+data.replies[i].meta.created_at+'</time></h5><p class="list-group-item-text">'+data.replies[i].content+'</p><button type="button" class="reply btn btn-default btn-xs" >reply</button><form action="/reply/store" method="post" role="form" class="replyForm hidden"><input type="hidden" name="from" value=""><input type="hidden" name="toWho" value='+data.replies[i].from._id+'><input type="hidden" name="toWhichComment" value='+cid+'><input type="hidden" name="toWhichReply" value='+data.replies[i]._id+'><div class="form-group"><textarea name="content" placeholder="Please input your comment here" required="" class="form-control"></textarea></div><b></b> <button type="button" class="replyBtn btn btn-primary btn-xs" isToReply="yes">submit</button></form></li>';
+                            $("#replies-list").append(li);
+                        }
+                    }else{
+                        for(var i=0;i<data.replies.length;i++){
+                            li='<li class="list-group-item"><h5 class="list-group-item-heading"><b>'+data.replies[i].from.name+'</b> replied to <b>'+data.replies[i].toWho.name+'</b><time class="pull-right">'+data.replies[i].meta.created_at+'</time></h5><p class="list-group-item-text">'+data.replies[i].content+'</p></li>';
+                            $("#replies-list").append(li);
+                        }
                     }
                 }
             }
