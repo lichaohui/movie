e2536aa7796eb94b138d11948cae6fde27cd3a6d
lichaohui <158109640@qq.com>
2016-09-06 16:54:09 +0800

    d

--- app/routes/route.js
+++ app/routes/route.js
@@ -43,9 +43,7 @@ module.exports=function(app){
     //展示用户主页的路由
     app.get('/user',user.index);
     //展示某个用户所有评论的路由
-    app.get('/user/comments',function(req,res){
-        res.end('hello');
-    });
+    app.get('/user/comments',comment.index);
     
     //展示某个电影下更多评论的路由
     app.get('/comment/more',comment.viewMore);
