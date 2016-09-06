c6acd0a5080494d0a5c7a2571b18b20eb01ed7f5
lichaohui <158109640@qq.com>
2016-09-06 11:30:10 +0800

    d

--- app/controllers/home/comment.js
+++ app/controllers/home/comment.js
@@ -41,3 +41,11 @@ exports.store=function(req,res){
         }; 
     });
 };
+
+//获取某个用户所有评论的方法
+exports.index=function(req,res){
+    var uid=req.query.uid;
+    comment.findByUser(uid,function(err,data){
+        res.render('home/user/comment',{'title':'我的评论','comment':data});
+    });
+};
