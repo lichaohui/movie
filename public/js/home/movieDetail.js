6769e63d5ef023a535c390ce0613e12b65e9c504
lichaohui <158109640@qq.com>
2016-09-07 16:35:59 +0800

    d

--- public/js/home/movieDetail.js
+++ public/js/home/movieDetail.js
@@ -41,21 +41,6 @@ $(function(){
         $(this).addClass('hidden');
     });
     
-    //格式化日期时间的函数
-    function date2str(x, y) {
-       var z = {
-          y: x.getFullYear(),
-          M: x.getMonth() + 1,
-          d: x.getDate(),
-          h: x.getHours(),
-          m: x.getMinutes(),
-          s: x.getSeconds()
-       };
-       return y.replace(/(y+|M+|d+|h+|m+|s+)/g, function(v) {
-          return ((v.length > 1 ? "0" : "") + eval('z.' + v.slice(-1))).slice(-(v.length > 2 ? v.length : 2))
-       });
-    }
-    
     //展示和收起回复表单
     $(document).on("click",".reply",function(){
         var userid=$("#userid").val();
