8c0c9cca615f2b0f4e3fb20e49474eae978e84fc
lichaohui <158109640@qq.com>
2016-09-07 15:59:28 +0800

    d

--- public/js/home/comment.js
+++ public/js/home/comment.js
@@ -1,6 +1,8 @@
 $(function(){
     //查看回复列表
     $('.viewreply').click(function(){
+        $("#totalReply").attr('id','');
+        $(this).attr('id','totalReply');
         var cid=$(this).attr('data-cid');
         $("#replies-list").empty();
         $.get('/reply/index',{"cid":cid},function(data,status){
@@ -34,4 +36,39 @@ $(function(){
             $(this).siblings('.replyForm').addClass('hidden');
         }
     }); 
+    
+    //异步提交回复
+    $(document).on('click',".replyBtn",function(){
+        var con=$(this).parent('.replyForm').find("[name='content']").val();
+        if(con==""){
+            alert("请先填写回复内容！");
+            $(this).parent('.replyForm').find("[name='content']").focus();
+        }else{
+            if($(this).attr('isToReply')=="yes"){
+                $(this).parent('.replyForm').ajaxSubmit({
+                    type:'post',
+                    url:'/reply/store',
+                    success:function(data){
+                        var rep='<li class="list-group-item"><h5 class="list-group-item-heading"><b>'+data.reply.from.name+'</b> replied to <b>'+data.reply.toWho.name+'</b><time class="pull-right">'+date2str(new Date(), "yyyy-MM-d h:m:s")+'</time></h5><p class="list-group-item-text">'+data.reply.content+'</p><button type="button" class="reply btn btn-default btn-xs">reply '+data.reply.from.name+'</button><form action="/reply/store" method="post" role="form" class="replyForm hidden"><input type="hidden" name="from" value=""><input type="hidden" name="toWho" value='+data.reply.from._id+'><input type="hidden" name="toWhichComment" value='+data.reply.toWhichComment+'><input type="hidden" name="toWhichReply" value='+data.reply.toWhichReply+'><div class="form-group"><textarea name="content" placeholder="Please input your comment here" required="" class="form-control"></textarea></div><button type="button" class="replyBtn btn btn-primary btn-xs" istoreply="yes">submit</button></form></li>';
+                        $("#replies-list").prepend(rep);
+                        $("#totalReply").text('view replies ('+data.totalReply+')');
+                    }
+                })
+            }else{
+                $("#replyFlag").attr('id','');
+                $(this).parentsUntil('#comments').find('.panel-body').attr('id','replyFlag');
+                $("#totalReply").attr('id','');
+                $(this).parentsUntil(".panel").find('.viewreply').attr('id','totalReply');
+                $(this).parent('.replyForm').ajaxSubmit({
+                    type:'post',
+                    url:'/reply/store',
+                    success:function(data){
+                        var rep='<ul class="list-group"><li class="list-group-item"><h5 class="list-group-item-heading"><b>'+data.reply.from.name+' </b>replied to<b> '+data.reply.toWho.name+'</b><time class="pull-right">'+date2str(new Date(), "yyyy-MM-d h:m:s")+'</time></h5><p class="list-group-item-text">'+data.reply.content+'</p></li></ul>';
+                        $("#replyFlag").append(rep);
+                        $("#totalReply").text('view replies ('+data.totalReply+')');
+                    }
+                })
+            }
+        }           
+    });
 })
