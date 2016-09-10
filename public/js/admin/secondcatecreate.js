ff1f7072c437708b14963a4d7160b78d33a80fb9
lichaohui <158109640@qq.com>
2016-09-10 16:20:54 +0800

    test

--- public/js/admin/secondcatecreate.js
+++ public/js/admin/secondcatecreate.js
@@ -1,6 +1,6 @@
 $(function(){
     //页面加载的时候将所有一级分类加载下拉选择框中
-    $.get('/admin/firstcate',{'isAjax':true},function(data.status){
+    $.get('/admin/firstcate',{'isAjax':true},function(data,status){
         var opt;
         for(var i=0;i<data.length;i++){
             opt='<option value='+data[i]._id+'>'+data[i].name+'</option>';
