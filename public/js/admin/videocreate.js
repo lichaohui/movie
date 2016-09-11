1c7c2251a3a58bf50b579e50e6ba99f0d64f5134
lichaohui <158109640@qq.com>
2016-09-11 15:11:45 +0800

    test

--- public/js/admin/videocreate.js
+++ public/js/admin/videocreate.js
@@ -18,5 +18,24 @@ $(function(){
                 }
             });
         }
-    })
+    });
+    
+    
+    var client = new OSS.Wrapper({
+        region: 'oss-cn-shanghai',
+        accessKeyId: '36rmkj8Ohcg1k7vR',
+        accessKeySecret: 'BKXxwSfX4bVTRhCxU5yj99KDDIKV7a',
+        bucket: 'xuefengoss'
+    });
+    
+    document.getElementById('playbill').addEventListener('change', function (e) {
+        var file = e.target.files[0];
+        var storeAs = 'upload-file';
+        console.log(file.name + ' => ' + storeAs);
+        client.multipartUpload(storeAs, file).then(function (result) {
+            console.log(result);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
 })
