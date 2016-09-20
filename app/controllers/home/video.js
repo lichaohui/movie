ce438bbc54d6d7cc584548fadd4dbf2c9ce52cae
lichaohui <158109640@qq.com>
2016-09-20 20:53:29 +0800

    test

--- app/controllers/home/video.js
+++ app/controllers/home/video.js
@@ -9,33 +9,21 @@ var comment=require('../../models/comment');
 
 //显示video列表的方法
 exports.index=function(req,res){
-    
+    var callback=function(err,data){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('home/video/index',{'title':'video','videos':data});
+        }
+    }
     //如果没有按分类搜索
     if(req.query.firstcate==null && req.query.secondcate==null){
         //调用video模型的fetch方法遍历数据传递给前台展示
-        video.fetch(function(err,data){
-            if(err){
-                console.log(err);
-            }else{
-                res.render('home/video/index',{'title':'video','videos':data});
-            }
-        });
+        video.fetch(callback);
     }else if(req.query.secondcate==null){
-        video.findByFirstcate(req.query.firstcate,function(err,data){
-            if(err){
-                console.log(err);
-            }else{
-                res.render('home/video/index',{'title':'video','videos':data});
-            }
-        });
+        video.findByFirstcate(req.query.firstcate,callback);
     }else{
-        video.findBySecondcate(req.query.secondcate,function(err,data){
-            if(err){
-                console.log(err);
-            }else{
-                res.render('home/video/index',{'title':'video','videos':data});
-            }
-        });
+        video.findBySecondcate(req.query.secondcate,callback);
     }
 };
 
