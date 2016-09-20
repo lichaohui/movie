a6d766e18d1b607fdfc8a2dc744a5df62ca533e4
lichaohui <158109640@qq.com>
2016-09-20 19:33:10 +0800

    test

--- app/routes/route.js
+++ app/routes/route.js
@@ -18,6 +18,7 @@ module.exports=function(app){
          */
         app.locals.user=req.session.user;
         app.locals.firstcates=req.session.firstcates;
+        console.log(app.locals.firstcates);
         next();
     });
     
