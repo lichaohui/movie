f76214b943f7e897a1d15abe475b38b432bcb748
lichaohui <158109640@qq.com>
2016-09-21 22:04:53 +0800

    test

--- app/controllers/home/secondcate.js
+++ app/controllers/home/secondcate.js
@@ -4,6 +4,8 @@ var secondcate=require('../../models/secondcate');
 
 //查找同个一级分类下所有二级分类的方法
 exports.query=function(req,res,next){
+    //删除session
+    delete req.session.secondcates;
     var pid=req.query.firstcate;
     secondcate.findByParent(pid,function(err,data){
         if(err){
