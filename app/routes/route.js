cd7d712005c044422078f90628baf79379bfeade
lichaohui <158109640@qq.com>
2016-09-24 10:10:48 +0800

    test

--- app/routes/route.js
+++ app/routes/route.js
@@ -12,16 +12,16 @@ module.exports=function(app){
      * 定义一个中间件，
      * 所有路由被执行之前都会先执行这个中间件
      */
-    //app.use(firstcate.query);
-    //app.use(function(req,res,next){
+    app.use(firstcate.query);
+    app.use(function(req,res,next){
         /*
          * 将session信息存入本地的变量中
          * 这样在模板张就可以使用这些变量了
          */
-    //    app.locals.user=req.session.user;
-      //  app.locals.firstcates=req.session.firstcates;
-        //next();
-//    });
+        app.locals.user=req.session.user;
+        app.locals.firstcates=req.session.firstcates;
+        next();
+    });
     
     /*
      * 设置“/”前台首页路由
