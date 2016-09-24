05f763112c4a40f29b746c873a20e3f7c50930f0
lichaohui <158109640@qq.com>
2016-09-24 19:08:21 +0800

    test

--- app/controllers/common/secondcate.js
+++ app/controllers/common/secondcate.js
@@ -5,39 +5,24 @@ var secondcate=require('../../models/secondcate');
 //查找同个一级分类下所有二级分类的方法
 exports.query=function(req,res,next){
     //公共的回调函数
-    /*var callback=function(err,data){
+    var callback=function(err,data){
         if(err){
             console.log(err);
         }else{
-            req.session.firstcate=pid;
             req.session.secondcates=data;
             next();
         }
-    };*/
+    };
     
     //删除session
     delete req.session.secondcates;
     if(req.query.firstcate==null){
         //如果请求参数中没有一级分类和二级分类则返回所有的二级分类
-        secondcate.fetch(function(err,data){
-            if(err){
-                console.log(err);
-            }else{
-                req.session.secondcates=data;
-                next();
-            }
-        });
+        secondcate.fetch(callback);
     }else{
         //如果请求参数中有一级分类则返回该一级分类下的所有二级分类
         var pid=req.query.firstcate;
-        secondcate.findByParent(pid,function(err,data){
-            if(err){
-                console.log(err);
-            }else{
-                req.session.firstcate=pid;
-                req.session.secondcates=data;
-                next();
-            }
-        });
+        req.session.firstcate=pid;
+        secondcate.findByParent(pid,callback);
     }  
 };
