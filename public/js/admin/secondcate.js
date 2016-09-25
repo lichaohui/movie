28d09f21fdb71b0f75338f98678045d8b7375cb1
lichaohui <158109640@qq.com>
2016-09-25 17:37:36 +0800

    test

--- public/js/admin/secondcate.js
+++ public/js/admin/secondcate.js
@@ -11,7 +11,8 @@ $(function(){
      */
     var scate=$("#scate").val();
     alert(scate);
-    $("#parent").find('option[value='+scate+']').attr("selected",true);
+    //$("#parent").find('option[value='+scate+']').attr("selected",true);
+    $("#parent").find('option').eq(0).attr("selected",true);
     alert('hello');
     
     /*----异步编辑二级分类----*/
