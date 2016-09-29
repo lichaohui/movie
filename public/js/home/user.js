428e0a87dca1b403c1b0035a61f41dad24bb2070
lichaohui <158109640@qq.com>
2016-09-29 16:47:55 +0800

    test

--- public/js/home/user.js
+++ public/js/home/user.js
@@ -3,20 +3,24 @@ $(function(){
     
     /*----点击提交按钮时候通过jquery.form.js插件异步提交表单----*/
     $("#sub").click(function(){
-        var url=$("#form").attr('action');
-        $("#form").ajaxSubmit({
-            type:'post',
-            url:url,
-            success:function(data){
-                if(data.isError){
-                    $("#warning").text(data.message).removeClass('hidden');
-                }else{
-                    $("#warning").addClass('hidden');
-                    $("#sub").attr('disabled','disabled');
-                    $("#success").text(data.message).removeClass('hidden');
-                    setTimeout(function(){window.location.href="/";},2000);
-                }
-            },
-        });
+        if($("#name").val==''||$("#password").val==''){
+            $("#warning").text('用户名和密码都不能为空').removeClass('hidden');
+        }else{
+            var url=$("#form").attr('action');
+            $("#form").ajaxSubmit({
+                type:'post',
+                url:url,
+                success:function(data){
+                    if(data.isError){
+                        $("#warning").text(data.message).removeClass('hidden');
+                    }else{
+                        $("#warning").addClass('hidden');
+                        $("#sub").attr('disabled','disabled');
+                        $("#success").text(data.message).removeClass('hidden');
+                        setTimeout(function(){window.location.href="/";},2000);
+                    }
+                },
+            });
+        }
     });
 })
