9a4ca963b4cb1dca3b19454d9294747e00fb8050
lichaohui <158109640@qq.com>
2016-10-14 16:24:58 +0800

    test

--- app/controllers/home/vcode.js
+++ app/controllers/home/vcode.js
@@ -1,21 +1,23 @@
 var ccap = require('ccap');
 exports.index=function(req,res){
     var captcha = ccap({
-      width:190,
-      height:50,　
-      offset:30,
-      quality:100,
-      fontsize:40,
-      /*generate:function(){
-        //自定义生成字符串
-
-        //此方法可不要
-               var str = "qQ";
-               return str;
-      }*/
+        width:190,
+        height:50,　
+        offset:30,
+        quality:100,
+        fontsize:40,
+        /*
+         * 自定义生成字符串，
+         * 此方法可不要
+         * 如果不要则系统会生成默认的随机字符串
+         */
+        /*generate:function(){
+            var str = "自定义的字符串";
+            return str;
+        }*/
     });
     var ary = captcha.get();
-    console.log(ary[0]);//字符串
+    console.log(ary);//字符串
     res.write(ary[1]); //
     res.end();
 }
