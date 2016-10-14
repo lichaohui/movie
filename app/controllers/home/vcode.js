a8ed6573e4de3679284b5407f19987a8b2a38fc3
lichaohui <158109640@qq.com>
2016-10-14 16:12:03 +0800

    test

--- app/controllers/home/vcode.js
+++ app/controllers/home/vcode.js
@@ -1,17 +1,24 @@
 //引入ccap模块用来生成图片验证码
 var ccap = require('ccap');
+var captcha1 = ccap();
+var captcha2 = ccap(width, height, offset);
 
 exports.index=function(){
-    var captcha1 = ccap();
-    var captcha2 = ccap(width, height, offset);
-    var captcha3 = ccap({
-        width:256,//set width,default is 256
-        height:60,//set height,default is 60
-        offset:40,//set text spacing,default is 40
-        quality:100,//set pic quality,default is 50
-        generate:function(){//Custom the function to generate captcha text
-             //generate captcha text here
-             return text;//return the captcha text
+    ccap({
+        //set width,default is 256
+        width:256,
+        //set height,default is 60
+        height:60,
+        //set text spacing,default is 40
+        offset:40,
+        //set pic quality,default is 50
+        quality:100,
+        //Custom the function to generate captcha text
+        generate:function(){
+            //generate captcha text here
+            //return the captcha text
+            console.log(text);
+            return text;
         }
     });
 }
