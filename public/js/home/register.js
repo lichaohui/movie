e40487438b908a3261f7a33806f00e527469886a
lichaohui <158109640@qq.com>
2016-10-20 21:09:52 +0800

    test

--- public/js/home/register.js
+++ public/js/home/register.js
@@ -1,14 +1,15 @@
 $(function(){
-    //异步提交用户输入的手机号
-    $("#checkunique").click(function(){
-        var phone=$("#phone").val();
-        var captcha=$("#captcha").val();
-        if(phone==''){
-            alert('请输入手机号！');
-            $("#phone").focus();
+    //异步提交用户输入的手机号或邮箱
+    $(".checkunique").click(function(){
+        var way=$(this).attr('data-way');
+        var pe=$("#"+way).val();
+        var captcha=$(this).parentsUntil("form").find('input[name=captcha]').val();
+        if(pe==''){
+            alert('请输入手机或者邮箱！');
+            $("#"+way).focus();
         }else if(captcha==''){
             alert('请输入验证码！');
-            $("#captcha").focus();
+            $(this).parentsUntil("form").find('input[name=captcha]').focus();
         }else{
             //$()
         }
