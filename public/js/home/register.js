9db54a006682d8bf9dc3c82ae9733e3f18141f59
lichaohui <158109640@qq.com>
2016-10-20 21:26:01 +0800

    test

--- public/js/home/register.js
+++ public/js/home/register.js
@@ -2,15 +2,14 @@ $(function(){
     //异步提交用户输入的手机号或邮箱
     $(".checkunique").click(function(){
         var way=$(this).attr('data-way');
-        alert(way);
         var pe=$("#"+way).val();
-        var captcha=$(this).parentsUntil("form").find('input[name=captcha]').val();
+        var captcha=$(this).parentsUntil(".tab-content").find('input[name=captcha]').val();
         if(pe==''){
             alert('请输入手机或者邮箱！');
             $("#"+way).focus();
         }else if(captcha==''){
             alert('请输入验证码！');
-            $(this).parentsUntil("form").find('input[name=captcha]').focus();
+            $(this).parentsUntil(".tab-content").find('input[name=captcha]').focus();
         }else{
             //$()
         }
