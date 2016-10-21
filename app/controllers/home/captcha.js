904685a36279897fff00dc19f1e53998848d711e
lichaohui <158109640@qq.com>
2016-10-21 13:23:08 +0800

    test

--- app/controllers/home/captcha.js
+++ app/controllers/home/captcha.js
@@ -20,7 +20,7 @@ exports.index=function(req,res){
             str=str.substr(0,4);
             //将验证码存储到session中
             req.session.captcha=str;
-            console.log(req.session.captcha);
+            console.log('luy'+str);
             //返回生成的字符串
             return str;
         }
