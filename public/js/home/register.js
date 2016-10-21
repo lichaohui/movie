22a251641f7f612821b34120d35d320ba7664ec8
lichaohui <158109640@qq.com>
2016-10-21 10:57:29 +0800

    test

--- public/js/home/register.js
+++ public/js/home/register.js
@@ -1,6 +1,7 @@
 $(function(){
     //异步提交用户输入的手机号或邮箱
     $(".checkunique").click(function(){
+        //通过属性判断用户是通过哪种方式进行注册的
         var way=$(this).attr('data-way');
         var pe=$("#"+way).val();
         var captcha=$(this).parentsUntil(".tab-content").find('input[name=captcha]').val();
@@ -11,7 +12,25 @@ $(function(){
             alert('请输入验证码！');
             $(this).parentsUntil(".tab-content").find('input[name=captcha]').focus();
         }else{
-            //$()
+            /*
+             * 如果用户输入的数据都合法
+             * 则进行表单的异步提交
+             */
+            var url=$(this).parents("form").attr('action');
+            $(this).parentsUntil(".tab-content").ajaxSubmit({
+                type:'post',
+                url:url,
+                success:function(data){
+                    if(data.isError){
+                        $("#warning").text(data.message).removeClass('hidden');
+                    }else{
+                        $("#warning").addClass('hidden');
+                        $(".checkunique").attr('disabled','disabled');
+                        $("#success").text(data.message).removeClass('hidden');
+                        //setTimeout(function(){window.location.href="/";},2000);
+                    }
+                },
+            })
         }
     })
 })
