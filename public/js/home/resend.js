6345da283549556cab9996127c4c558c97f52562
lichaohui <158109640@qq.com>
2016-10-22 16:40:49 +0800

    test

--- public/js/home/resend.js
+++ public/js/home/resend.js
@@ -1,12 +1,17 @@
 //重新发送短信/邮箱验证码的js
-var time=60;
-var t=window.setInterval(function(){
-    time--;
-    document.getElementById("count").innerHTML=time;
-},1000);
+function countDown(time){
+    var t=window.setInterval(function(){
+        time--;
+        while(time>1){
+            document.getElementById("count").innerHTML=time;
+        }else{
+            window.clearInterval(t);
+            document.getElementById("count").setAttribute('disabled',false);
+        }
+    },1000);
+}
 
 
 $(function(){
-    
-    
+    countDown(60);
 })
