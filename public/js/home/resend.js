ef78841699b070dc59b018d17e3779987ad5ee6d
lichaohui <158109640@qq.com>
2016-10-22 16:36:19 +0800

    test

--- public/js/home/resend.js
+++ public/js/home/resend.js
@@ -1,10 +1,10 @@
 //重新发送短信/邮箱验证码的js
 var time=60;
-var t=setTimeout(function(){
+var t=window.setInterval(function(){
     time--;
     document.getElementById("count").innerHTML=time;
 },1000);
-window.onload=t;
+
 
 $(function(){
     
