dd695f56240a6bf27c13f235671ce66ca43fe230
lichaohui <158109640@qq.com>
2016-10-24 19:55:32 +0800

    test

--- public/js/home/user.js
+++ public/js/home/user.js
@@ -8,16 +8,22 @@ $(function(){
         if(name==''||password==''){
             $("#warning").text('用户名和密码都不能为空').removeClass('hidden');
         }else{
+            //如果用户提交的用户名和密码都不为空则通过一个ajax异步提交表单
+            //首先将按钮置为不可点击的状态
+            $(this).text("请稍后").attr('disabled',true);
+            //发送ajax请求异步提交表单数据
             var url=$("#form").attr('action');
             $("#form").ajaxSubmit({
                 type:'post',
                 url:url,
                 success:function(data){
                     if(data.isError){
+                        //如果有错误则显示返回的错误信息并将提交按钮重新设置可点击的状态
                         $("#warning").text(data.message).removeClass('hidden');
+                        $("#sub").attr('disabled',false);
                     }else{
+                        //如果成功则显示成功信息并进行页面的跳转
                         $("#warning").addClass('hidden');
-                        $("#sub").attr('disabled','disabled');
                         $("#success").text(data.message).removeClass('hidden');
                         setTimeout(function(){window.location.href="/";},2000);
                     }
