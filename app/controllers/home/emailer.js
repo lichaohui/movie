8ce30425e5b8b0261841b9cec5462a899a1c4cfd
lichaohui <158109640@qq.com>
2016-10-24 11:37:49 +0800

    test

--- app/controllers/home/emailer.js
+++ app/controllers/home/emailer.js
@@ -2,30 +2,27 @@
 var nodemailer = require("nodemailer");
 
 exports.send=function(req,res){
-    // 开启一个 SMTP 连接池
-    var smtpTransport = nodemailer.createTransport("SMTP",{
-      host: "smtp.163.com", // 主机
-      secureConnection: true, // 使用 SSL
-      port: 465, // SMTP 端口
-      auth: {
-        user: "17076467717@163.com", // 账号
-        pass: "misswife19900214" // 密码
-      }
-    });
-    // 设置邮件内容
-    var mailOptions = {
-      from: "学疯网", // 发件地址
-      to: "158109640@qq.com", // 收件列表
-      subject: "学疯网会员验证", // 标题
-      html: "<b>thanks a for visiting!</b> 世界，你好！" // html 内容
-    }
-    // 发送邮件
-    smtpTransport.sendMail(mailOptions, function(error, response){
-      if(error){
-        console.log(error);
-      }else{
-        console.log("Message sent: " + response.message);
-      }
-      smtpTransport.close(); // 如果没用，关闭连接池
-    });
+    var transporter = nodemailer.createTransport({  
+        service: '163',  
+        auth: {  
+            user: '17076467717@163.com',  
+            pass: 'misswife19900214' //授权码,通过QQ获取  
+        }  
+    });  
+    var mailOptions = {  
+        from: '17076467717@163.com', // 发送者  
+        to: '158109640@qq.com', // 接受者,可以同时发送多个,以逗号隔开  
+        subject: 'nodemailer2.5.0邮件发送', // 标题  
+        //text: 'Hello world', // 文本  
+        html: `<h2>nodemailer基本使用:</h2><h3>  
+        <a href="http://blog.csdn.net/zzwwjjdj1/article/details/51878392">  
+        http://blog.csdn.net/zzwwjjdj1/article/details/51878392</a></h3>`   
+    };  
+    transporter.sendMail(mailOptions, function (err, info) {  
+        if (err) {  
+            console.log(err);  
+            return;  
+        }  
+        console.log('发送成功');  
+    });  
 }
