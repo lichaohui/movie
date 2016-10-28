3c9c01fde6b31517fe620db267c6586b09e76426
lichaohui <158109640@qq.com>
2016-10-28 19:56:42 +0800

    test

--- public/js/home/udata.js
+++ public/js/home/udata.js
@@ -13,6 +13,7 @@ $(function(){
              */
             $("#form").attr({'action':'/usermsg/'+uid,'method':'put'});
             $('#avathum').attr({'title':'点击更换头像','src':data.usermsg.avatar});
+            $('#avatar').val(data.usermsg.avatar);
             $('#name').val(data.usermsg.name);
             $('input[name=sex][value='+data.usermsg.sex+']').attr('checked',true);
             $("#position").find('option[value='+data.usermsg.position+']').attr("selected",true);
