a49c7b688056917b49a654106f785945eb8d8458
lichaohui <158109640@qq.com>
2016-10-28 16:09:45 +0800

    test

--- public/js/home/udata.js
+++ public/js/home/udata.js
@@ -8,7 +8,6 @@ $(function(){
         if(data.isError){
             alert(data.message);
         }else{
-            alert(data.usermsg.avatar);
             //如果成功获取了用户信息则将用户信息遍历到页面中
             if(data.usermsg.avatar){
                 alert('有头像');
