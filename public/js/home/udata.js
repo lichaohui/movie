66d15932a41c82bae61566b054c523738097104e
lichaohui <158109640@qq.com>
2016-10-29 10:54:38 +0800

    test

--- public/js/home/udata.js
+++ public/js/home/udata.js
@@ -48,4 +48,20 @@ $(function(){
             },
         });
     });
+    
+    $('#image-cropper').cropit();
+
+// When user clicks select image button,
+// open select file dialog programmatically
+$('.select-image-btn').click(function() {
+  $('.cropit-image-input').click();
+});
+
+// Handle rotation
+$('.rotate-cw-btn').click(function() {
+  $('#image-cropper').cropit('rotateCW');
+});
+$('.rotate-ccw-btn').click(function() {
+  $('#image-cropper').cropit('rotateCCW');
+});
 })
