911463a9bb36103f40b1b9fe800391ac1e7146ba
lichaohui <158109640@qq.com>
2016-10-30 15:42:13 +0800

    test

--- app/controllers/home/avatar.js
+++ app/controllers/home/avatar.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var	imageMagick = gm.subClass({ imageMagick : true });
 exports.crop = function(req, res) {
 	res.header('Content-Type', 'text/plain');
-    console.log(req.body);
+    console.log(req.params);
     var path = req.files.orimage.path;	//获取用户上传过来的文件的当前路径
     imageMagick(path).resize(150, 150, '!').autoOrient().write('public/images/user/'+req.files.img.name, function(err){
         if (err){
