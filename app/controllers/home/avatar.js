a5f6a543f207a6acec73a77a6823e15f4728982d
lichaohui <158109640@qq.com>
2016-10-30 15:17:27 +0800

    test

--- app/controllers/home/avatar.js
+++ app/controllers/home/avatar.js
@@ -5,11 +5,11 @@ exports.upload = function(req, res) {
 	res.header('Content-Type', 'text/plain');
     var path = req.files.img.path;	//获取用户上传过来的文件的当前路径
     imageMagick(path).resize(150, 150, '!').autoOrient().write('public/images/user/'+req.files.img.name, function(err){
-        if (err) {
+        if (err){
             console.log(err);
             res.end();
         }else{
             console.log('图片保存成功');
         }
-    }
+    })
 };
