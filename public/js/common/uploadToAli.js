a4c82a5fc2c9b9c63ea1c7d4a274ddc34ef5bcc4
lichaohui <158109640@qq.com>
2016-10-31 10:03:19 +0800

    test

--- public/js/common/uploadToAli.js
+++ public/js/common/uploadToAli.js
@@ -12,6 +12,7 @@ function uploadFileToAlioss(e,path,callback){
      * 设置上传文件保存到阿里oss的名称为时间戳加后缀名
      * path表示阿里oss中的我们自建的文件夹
      */
+    var oldtxt=$('#sub').text();
     var uploadName=path+new Date().getTime()+'.'+format;
     $("#sub").text('正在上传中').attr("disabled",true);
     //上传文件
@@ -23,7 +24,7 @@ function uploadFileToAlioss(e,path,callback){
          */
         var url='http://'+client.options.bucket+'.'+client.options.region+'.'+'aliyuncs.com/'+result.name;
         callback(url);
-        $("#sub").text('submit').attr("disabled",false);
+        $("#sub").text(oldtxt).attr("disabled",false);
     }).catch(function (err) {
         console.log(err);
     });
