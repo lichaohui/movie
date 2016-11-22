dbc279956ff9638272fe6e3211b2feb72858b6b5
lichaohui <158109640@qq.com>
2016-11-22 21:33:39 +0800

    test

--- public/js/home/account.js
+++ public/js/home/account.js
@@ -5,14 +5,14 @@ $(function(){
 				required: true,
 				email:true
 			},
-			vcode: "required"
+			ecode: "required"
 		},
 		messages: {
 			email:{
 				required:'请输入邮箱',
 				phone:'请输入正确格式的邮箱'
 			},
-			vcode: "请输入邮箱动态验证码",
+			ecode: "请输入邮箱动态验证码",
 		},
     });
 })
