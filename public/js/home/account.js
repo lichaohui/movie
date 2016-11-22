01c6944df508afa7496361bba8817f4cc8e5abec
lichaohui <158109640@qq.com>
2016-11-22 21:57:36 +0800

    test

--- public/js/home/account.js
+++ public/js/home/account.js
@@ -1,4 +1,5 @@
 $(function(){
+    //jquery.validate验证邮箱表单
     $('#emailform').validate({
         rules: {
 			email: {
@@ -12,7 +13,19 @@ $(function(){
 				required:'请输入邮箱',
 				email:'请输入正确格式的邮箱'
 			},
-			ecode: "请输入邮箱动态验证码",
+			ecode: "请输入邮箱验证码",
 		},
+    });
+    //发送邮箱验证码
+    $('#sendecode').click(function(){
+        //先检验邮箱是否正确输入
+        var email=$('#email').val();
+        if(isemail(email)){
+            //如果邮箱格式输入正确则发送邮箱验证啊
+        }else{
+            //否则提示用户输入正确格式的邮箱
+            $('#email-error').text('请输入正确格式的邮箱');
+            $('#email').focus();
+        }
     });
 })
