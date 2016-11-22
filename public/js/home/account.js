3ac9e5f22ddb3131d4461368e364262f5103564e
lichaohui <158109640@qq.com>
2016-11-22 21:30:15 +0800

    test

--- public/js/home/account.js
+++ public/js/home/account.js
@@ -1,3 +1,18 @@
 $(function(){
-    $('#emailform').validate();
+    $('#emailform').validate(
+        rules: {
+			email: {
+				required: true,
+				email:true
+			},
+			vcode: "required"
+		},
+		messages: {
+			email:{
+				required:'请输入邮箱',
+				phone:'请输入正确格式的邮箱'
+			},
+			vcode: "请输入邮箱动态验证码",
+		},
+    );
 })
