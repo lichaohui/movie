35baecc6e26beff5d7b72f6fa6878fcd7b576d91
lichaohui <158109640@qq.com>
2016-11-24 22:29:31 +0800

    test

--- public/js/home/vlogin.js
+++ public/js/home/vlogin.js
@@ -19,8 +19,6 @@ $(function(){
             var oldTxt=$('#sub').text();
             $('#sub').text("请稍后..").attr('disabled',true);
             $(form).ajaxSubmit({
-                type:'post',
-                //url:'/dologin',
                 success:function(data){
                     if(data.isError){
                         //如果有错误返回则显示错误信息并将按钮置为可点击状态
@@ -34,45 +32,38 @@ $(function(){
             });
         }
     });
-    //异步提交用户输入的手机号或邮箱
-    $(".checkexist").click(function(){
-        //通过属性判断用户是通过哪种方式进行注册的
-        var way=$(this).attr('data-way');
-        var pe=$("#"+way).val();
-        var captcha=$(this).parentsUntil(".tab-content").find('input[name=captcha]').val();
-        /*
-         * 通过字符串is+变量way的方式组装一个字符串，
-         * 该字符串就是验check.js文件中验证各个格式的函数名
-         * 然后通过eval方法将表示函数名的字符串动态解析成js函数来执行
-         * 就实现了动态判断用户输入的是那种类型的数据来调用不同的函数进行验证的功能
-         */
-        if(eval('is'+way)(pe)==false){
-            $("#"+way).focus();
-        }else if(captcha==''){
-            alert('请输入验证码！');
-            $(this).parentsUntil(".tab-content").find('input[name=captcha]').focus();
-        }else{
-            /*
-             * 如果用户输入的数据都合法
-             * 则进行表单的异步提交
-             * 并将按钮置为不可点击的状态
-             */
-            $(this).text('请稍后...').attr('disabled',true);
-            var url=$(this).parents("form").attr('action');
-            $(this).parents("form").ajaxSubmit({
-                type:'post',
-                url:url,
+    
+    //jquery.validate验证邮箱验证登录表单
+    $('#emailform').validate({
+        rules: {
+			email: {
+                required:true,
+                email:true,
+            },
+            captcha:'required',
+		},
+		messages: {
+            email:{
+                required:'请输入邮箱',
+                phone:'请输入正确格式的邮箱'
+            },
+            captcha:'请输入验证码',
+		},
+        submitHandler:function(form) {
+            var oldTxt=$('#sub1').text();
+            $('#sub1').text("请稍后..").attr('disabled',true);
+            $(form).ajaxSubmit({
                 success:function(data){
                     if(data.isError){
                         //如果有错误返回则显示错误信息并将按钮置为可点击状态
-                        $("#warning-"+way).text(data.message).removeClass('hidden');
-                        $(".checkexist").text('下一步 >>').attr("disabled",false);
+                        $("#warning-email").text(data.message).removeClass('hidden');
+                        $("#sub1").text('下一步 >>').attr("disabled",false);
                     }else{
                         //提交成功后进入下一个验证登录页面
                         setTimeout(function(){window.location.href="/vlogin1?type="+data.type+'&val='+data.val;},1000);
                     }
-                },
-            })
+                }
+            });
         }
-    })
+    });
 })
