73cb91e4b2d3130cdbfad1149084f796d232a5dc
lichaohui <158109640@qq.com>
2016-11-24 22:18:24 +0800

    test

--- public/js/home/vlogin.js
+++ public/js/home/vlogin.js
@@ -1,4 +1,39 @@
 $(function(){
+    //jquery.validate验证手机验证登录表单
+    $('#phoneform').validate({
+        rules: {
+			phone: {
+                required:true,
+                phone:true,
+            },
+            captcha:'required',
+		},
+		messages: {
+            phone:{
+                required:'请输入手机号',
+                phone:'请输入正确格式的手机号'
+            },
+            captcha:'请输入验证码',
+		},
+        submitHandler:function(form) {
+            var oldTxt=$('#sub').text();
+            $('#sub').text("请稍后..").attr('disabled',true);
+            $(form).ajaxSubmit({
+                type:'post',
+                //url:'/dologin',
+                success:function(data){
+                    if(data.isError){
+                        //如果有错误返回则显示错误信息并将按钮置为可点击状态
+                        $("#warning-phone").text(data.message).removeClass('hidden');
+                        $("#sub").text('下一步 >>').attr("disabled",false);
+                    }else{
+                        //提交成功后进入下一个验证登录页面
+                        setTimeout(function(){window.location.href="/vlogin1?type="+data.type+'&val='+data.val;},1000);
+                    }
+                }
+            });
+        }
+    });
     //异步提交用户输入的手机号或邮箱
     $(".checkexist").click(function(){
         //通过属性判断用户是通过哪种方式进行注册的
