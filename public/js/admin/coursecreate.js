bfd2521d3ad55bcb8b91184670dc97ef427e7b92
lichaohui <158109640@qq.com>
2016-11-30 19:04:29 +0800

    test

---
+++ public/js/admin/coursecreate.js
@@ -0,0 +1,22 @@
+$(function(){
+    //根据选中的一级菜单ajax异步加载响应的二级菜单
+    $("#parentcate").change(function(){
+        $("#secondcate").empty();
+        var pid=$(this).val();
+        if(pid==""){
+            $("#secondcate").empty();
+        }else{
+            $.get('/admin/secondcate/query',{'pid':pid},function(data,status){
+                if(data.isError){
+                    alert(data.message);
+                }else{
+                    var opt;
+                    for(var i=0;i<data.length;i++){
+                        opt='<option value='+data[i]._id+'>'+data[i].name+'</option>';
+                        $("#secondcate").append(opt);
+                    }
+                }
+            });
+        }
+    });
+})
