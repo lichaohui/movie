8503a2c07ebf12e23fb8e453cb091cf36deee04b
lichaohui <158109640@qq.com>
2016-12-04 11:18:35 +0800

    test

--- public/js/home/course.js
+++ public/js/home/course.js
@@ -1,7 +1,6 @@
 $(function(){
     var $container = $('.masonry-container');
     $container.imagesLoaded( function () {
-        alert('执行了');
         $container.masonry({
             columnWidth: '.item',
             itemSelector: '.item'
