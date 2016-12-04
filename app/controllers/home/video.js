457e9888f9cdb0c31fe438edf79151c5558a6cfc
lichaohui <158109640@qq.com>
2016-12-04 15:48:18 +0800

    test

--- app/controllers/home/video.js
+++ app/controllers/home/video.js
@@ -30,7 +30,7 @@ exports.index=function(req,res){
             //从所有数据中返回当前页应有的数据
             var pageData=data.slice((page-1)*limit,page*limit);
             
-            res.render('home/video/index',{'title':'course','cid':cid,'videos':pageData,'condition':condition,'pageLength':pageLength,'curPage':page});
+            res.render('home/video/index',{'title':req.query.course,'cid':cid,'videos':pageData,'condition':condition,'pageLength':pageLength,'curPage':page});
         }
     };
     //获取请求参数中的课程id
