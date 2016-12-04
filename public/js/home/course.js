f68a08240880ae9a7dbde56ccdd936c001b622f9
lichaohui <158109640@qq.com>
2016-12-04 10:08:48 +0800

    test

---
+++ public/js/home/course.js
@@ -0,0 +1,9 @@
+$(function(){
+    var $container = $('.masonry-container');
+    $container.imagesLoaded( function () {
+        $container.masonry({
+        columnWidth: '.item',
+        itemSelector: '.item'
+        });
+    });
+})
