db94207aa8833bf9502419fb74b92ac3e7be2fc2
lichaohui <158109640@qq.com>
2016-12-05 20:05:41 +0800

    test

--- app/controllers/home/course.js
+++ app/controllers/home/course.js
@@ -8,7 +8,7 @@ var course=require('../../models/course');
 //显示course列表的方法
 exports.index=function(req,res){
     //设置每页的显示条数
-    var limit=4;
+    var limit=2;
     
     /*
      * 通过一个三元表达式来设置page
