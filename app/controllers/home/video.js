4c7dddc655fcdc246a2650d413569bddb673b9f0
lichaohui <158109640@qq.com>
2016-12-08 20:30:36 +0800

    test

--- app/controllers/home/video.js
+++ app/controllers/home/video.js
@@ -61,8 +61,6 @@ exports.show=function(req,res,next){
     var id=req.params.id;
     //通过id获取数据并将数据发送给前台视图
     video.findById(id,function(err,data){
-        req.params.cid=data.course;
-        req.params.queue=data.queue;
         req.params.video=data;
         next();
     });
@@ -70,15 +68,16 @@ exports.show=function(req,res,next){
 
 //播放指定video后操作learn表的方法
 exports.querylearn=function(req,res){
+    var video=req.params.video;
     //先通过用户的id和当前的课程id查询learn表中该用户有没有学习过该课程
-    learn.findByUC(req.session.user._id,req.params.cid,function(err,data){
+    learn.findByUC(req.session.user._id,video.course,function(err,data){
         if(data.length>0){
             /*
              * 如果data.length大于0
              * 则证明该用户学习过该课程
              * 那么久更新learn表中的上一次学习课时字段(lastque)
              */
-            data.lastque=req.params.queue;
+            data.lastque=video.queue;
             data.save(function(err,data){
                 if(err){
                     console.log(err);
@@ -91,7 +90,7 @@ exports.querylearn=function(req,res){
              */
             var newlearn=new learn({
                 'uid':req.session.user._id,
-                'lastque':req.params.queue,
+                'lastque':video.queue,
             });
             newlearn.save(function(err,data){
                 if(err){
@@ -100,6 +99,6 @@ exports.querylearn=function(req,res){
             })
         }
         //加载当前视频的评论并将数据返回给前台视图
-        res.render('home/video/detail',{'title':req.params.video.name,'video':req.params.video,'comments':req.params.comments}); 
+        res.render('home/video/detail',{'title':video.name,'video':video,'comments':req.params.comments}); 
     })
 }
